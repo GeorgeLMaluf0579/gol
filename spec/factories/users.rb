4e6174c340b1e2b85418eb844e8ab7512f080785
George Luiz 'Maverick' Maluf <georgelmaluf286@gmail.com>
2025-02-20 10:36:33 -0300

    feat: Add HomeController and User model tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { 'p@$$w0rd' }
+    password_confirmation { 'p@$$w0rd' }
   end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    context "with valid attributes" do
+      let(:user) { build(:user) }
+
+      it "is valid" do
+      expect(user).to be_valid
+      end
+    end
+
+    describe "with invalid attributes" do
+      context "with invalid email"  do
+        let(:invalid_user) { build(:user, email: 'abdc.com') }
+        it "is invalid" do
+          expect(invalid_user).not_to be_valid
+        end
+      end
+
+      context "with invalid password" do
+        let(:invalid_user) { build(:user, password: '12345', password_confirmation: '12345') }
+        it "is invalid" do
+          expect(invalid_user).not_to be_valid
+        end
+      end
+
+      context "with invalid confirmation_password" do
+        let(:invalid_user) { build(:user, password_confirmation: '123abcd') }
+        it "is invalid" do
+          expect(invalid_user).not_to be_valid
+        end
+      end
+    end
+  end
+end
